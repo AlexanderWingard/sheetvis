22af19cb8a6f77c2ea78ba2d5ebdd3253a23ccd5
Alexander Wingård <alexander.wingard@gmail.com>
2014-12-16 16:23:05 +0100

    Add support for any number of columns

--- getdata.js
+++ getdata.js
@@ -1,30 +1,60 @@
-var sheet = {data : [], index : "", cols : []}
+sheet = {data : [], index : "", cols : []}
 
 if(window.location.hash.length < 2) {
     display_error("No key specified")
 } else {
     var key = window.location.hash.slice(1)
     var url = "http://dev.axw.se/gdocs/" + key
-    d3.csv(url, function(d) {
+    d3.csv(url, function(error, csv){
+        if(error) {
+            display_error("Could not get data from " + url)
+            return
+        } else if (csv.length == 0) {
+            display_error("Could not fetch any rows")
+            return
+        }
+        parse_sheet(csv);
+        draw(sheet)
+    })
+}
+
+function parse_sheet(csv) {
+    // Find index col
+    var index;
+    var first = csv[0];
+    for (var key in first) {
+        if(/[-\/\\:]+/.test(first[key]) && !isNaN(Date.parse(first[key]))) {
+            index = key
+        }
+    }
+    if (index === undefined) {
+        display_error("Could not find any date column")
+        return
+    }
+
+    // Convert strings and list columns
+    var colset =  {}
+    csv.forEach(function(d) {
+        d[index] = new Date(d[index])
         for(var key in d) {
-            if(key.toUpperCase() == "DATE") {
-                d[key] = new Date(d[key])
-            } else if (key.toUpperCase() != "COMMENT") {
+            if (key !== index) {
+                colset[key] = true
                 d[key] = parseFloat(d[key])
             }
         }
-        return d
-    }, function(error, d){
-        if(error) {
-            display_error("Could not get data from " + url)
-        } else {
-            var index = "Date"
-            var cols = ["kH", "Ca", "Mg"]
-            var data = d.sort(function(a,b) {return a[index].getTime() - b[index].getTime()})
-            sheet = {data : data, index : index, cols : cols}
-            draw(sheet)
-        }
     })
+
+    // Remove empty columns
+    for(var col in colset) {
+        if(csv.every(function(row) { return isNaN(row[col]) })) {
+            delete colset[col];
+        }
+    }
+    // Sort data
+    csv.sort(function(a,b) {return a[index].getTime() - b[index].getTime()})
+
+    // Create sheet
+    sheet = {data : csv, index : index, cols : Object.keys(colset)}
 }
 
 function display_error(error) {
